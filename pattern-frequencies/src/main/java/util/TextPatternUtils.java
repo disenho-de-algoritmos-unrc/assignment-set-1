2bdbb3eb13b159097cfa776008182ec942051cd1
Nazareno Aguirre <naguirre@dc.exa.unrc.edu.ar>
2020-04-08 02:37:31 -0300

    Initial import of project 3 (most frequent k-size patterns).

---
+++ pattern-frequencies/src/main/java/util/TextPatternUtils.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.Set;
+
+/**
+ * Utilities for text pattern processing.
+ *
+ */
+public final class TextPatternUtils {
+
+    /**
+     * Default constructor of class.
+     * Made private to comply with style constraints.
+     */
+    private TextPatternUtils()  { }
+
+    /**
+     * Computes the most frequent patterns of size k in text.
+     * @param text is the text where to compute the patterns
+     * @param k is the size of the patterns to be computed
+     * @return the set of most frequent patterns of size k in text.
+     */
+    public static Set<String> mostFrequentPatterns(final String text, final int k) {
+        throw new UnsupportedOperationException("method not yet implemented");
+    }
+
+}
